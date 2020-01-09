5adda3e63647db33edcde3883009bf4f24a2b0fa
makhatadze <vitomaxatadze@gmail.com>
2020-01-09 12:35:47 +0400

    End the http request learn

--- req-HTTP/server.js
+++ req-HTTP/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const datas = [
+const todos = [
   {
     id: 1,
     text: 'DAta One'
@@ -15,23 +15,37 @@ const datas = [
   }
 ];
 const server = http.createServer((req, res) => {
-  //   res.statusCode = 404;
-  //   res.setHeader('Content-Type', 'application/json');
-  //   res.setHeader('X-Powered-By', 'Node.js');
-  // Same thing
-  res.writeHead(404, {
-    'Content-Type': 'application/json',
-    'X-Powered-By': 'Node.js'
-  });
+  const { method, url } = req;
+  let body = [];
+
+  req
+    .on('data', chunk => {
+      body.push(chunk);
+    })
+    .on('end', () => {
+      body = Buffer.concat(body).toString();
+
+      let status = 404;
+
+      const response = {
+        success: false,
+        data: null
+      };
+
+      if (method === 'GET' && url === '/todos') {
+        status = 200;
+        response.data = todos;
+      }
+
+      res.writeHead(status, {
+        'Content-Type': 'application/json',
+        'X-Powered-By': 'Node.js'
+      });
+      console.log(body);
+    });
 
   // we can return normal status code and data! also
-  res.end(
-    JSON.stringify({
-      success: false,
-      error: 'Not Found',
-      data: null
-    })
-  );
+  res.end(JSON.stringify(response));
 });
 
 const PORT = 5000;
