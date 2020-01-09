d11d71f5d3af14777643a880d361600baa7ddf26
makhatadze <vitomaxatadze@gmail.com>
2020-01-09 16:47:49 +0400

    IIIIIIIImportant commit asyncHandler

--- little-application/controllers/posts.js
+++ little-application/controllers/posts.js
@@ -1,101 +1,83 @@
 const Post = require('../models/Post')
 const ErrorResponse = require('../utils/errorResponse')
+const asyncHandler = require('../middleware/async')
 
 // @desc   Get all Posts
 // @route  Get /api/v1/posts
 // @access Public
-exports.getPosts = async (req, res, next) => {
-    try {
-        const post = await Post.find()
+exports.getPosts = asyncHandler(async (req, res, next) => {
 
-        if (!post) {
-            return next(new ErrorResponse(`Post not fount with id of ${req.params.id}`, 404));
-        }
-        res.status(200).json({
-            success: true,
-            data: post
-        })
-    } catch (err) {
-        next(err)
-    }
-}
+    const post = await Post.find()
+
+    res.status(200).json({
+        success: true,
+        data: post
+    })
+
+})
 
 // @desc   Get signle Post
 // @route  Get /api/v1/posts/:id
 // @access Public
-exports.getPost = async (req, res, next) => {
-    try {
-        const post = await Post.findById(req.params.id)
+exports.getPost = asyncHandler(async (req, res, next) => {
 
-        if (!post) {
-            return next(new ErrorResponse(`Post not fount with id of ${req.params.id}`, 404));
-        }
-        res.status(200).json({
-            success: true,
-            data: post
-        })
-    } catch (err) {
-        // next(new ErrorResponse(`Post not fount with id of ${req.params.id}`, 404));
-        next(err)
+    const post = await Post.findById(req.params.id)
+
+    if (!post) {
+        return next(new ErrorResponse(`Post not fount with id of ${req.params.id}`, 404));
     }
-}
+    res.status(200).json({
+        success: true,
+        data: post
+    })
+
+})
 
 // @desc   Create new Post
 // @route  Post /api/v1/posts/:id
 // @access Private
-exports.createPost = async (req, res, next) => {
-    try {
-        const post = await Post.create(req.body);
+exports.createPost = asyncHandler(async (req, res, next) => {
 
-        res.status(201).json({
-            success: true,
-            data: post
-        })
-    } catch (err) {
-        next(err)
-    }
-}
+    const post = await Post.create(req.body);
+
+    res.status(201).json({
+        success: true,
+        data: post
+    })
+
+})
 // @desc   Update  Post
 // @route  PUT /api/v1/posts/:id
 // @access Private
-exports.updatePost = async (req, res, next) => {
-
-    try {
-        const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        })
-        if (!post) {
-            return next(new ErrorResponse(`Post not fount with id of ${req.params.id}`, 404));
-        }
-        res.status(200).json({
-            success: true,
-            data: post
-        })
-    } catch (err) {
-        next(err)
+exports.updatePost = asyncHandler(async (req, res, next) => {
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    })
+    if (!post) {
+        return next(new ErrorResponse(`Post not fount with id of ${req.params.id}`, 404));
     }
+    res.status(200).json({
+        success: true,
+        data: post
+    })
 
-
-
-}
+})
 // @desc   Delete Post
 // @route  DELETE /api/v1/posts/:id
 // @access Private
-exports.deletePost = async (req, res, next) => {
-    try {
-        const post = await Post.findByIdAndDelete(req.params.id)
+exports.deletePost = asyncHandler(async (req, res, next) => {
 
-        if (!post) {
-            return next(new ErrorResponse(`Post not fount with id of ${req.params.id}`, 404));
-        }
+    const post = await Post.findByIdAndDelete(req.params.id)
 
-        res.status(200).json({
-            success: true,
-            count: post.length,
-            data: {}
-        })
-    } catch (err) {
-        next(err)
+    if (!post) {
+        return next(new ErrorResponse(`Post not fount with id of ${req.params.id}`, 404));
     }
-}
+
+    res.status(200).json({
+        success: true,
+        count: post.length,
+        data: {}
+    })
+
+})
