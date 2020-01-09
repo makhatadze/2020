92b8d247f9c7fcb90bdb1f31a0fa461ea2126c72
makhatadze <vitomaxatadze@gmail.com>
2020-01-09 14:26:02 +0400

    add mongose and create atlas connection

--- little-application/server.js
+++ little-application/server.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
 const morgan = require('morgan');
+const connectDB = require('./config/db')
+
+// Load env vars
+dotenv.config({
+    path: './config/config.env'
+});
+
+// Connect Mongodb
+connectDB()
+
 
 // Route
 const posts = require('./routes/posts');
 
-// Load env vars
-dotenv.config({
-  path: './config/config.env'
-});
 
 const app = express();
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+    app.use(morgan('dev'));
 }
 
 // Own middleware
@@ -27,7 +32,14 @@ app.use('/api/v1/posts', posts);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
-  PORT,
-  console.log(`Server running ${process.env.NODE_ENV} mode on port ${PORT}`)
+const server = app.listen(
+    PORT,
+    console.log(`Server running ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled promise  rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`)
+    // Close server & exit process
+    server.close(() => process.exit(1))
+})
