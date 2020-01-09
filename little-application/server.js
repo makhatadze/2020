25951b89c40ffe7500d19ca47f90c162ef9573d1
makhatadze <vitomaxatadze@gmail.com>
2020-01-09 13:46:10 +0400

    add morgan for middleware

--- little-application/server.js
+++ little-application/server.js
@@ -1,28 +1,33 @@
-const express = require('express')
-const dotenv = require('dotenv')
+const express = require('express');
+const dotenv = require('dotenv');
 
+const morgan = require('morgan');
 
 // Route
-const posts = require('./routes/posts')
+const posts = require('./routes/posts');
 
 // Load env vars
 dotenv.config({
-    path: './config/config.env'
-})
+  path: './config/config.env'
+});
 
-const app = express()
+const app = express();
 
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`)
-    next()
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
-app.use(logger)
+// Own middleware
+// const logger = require('./middleware/logger')
+// app.use(logger)
+
 // Mount roouters
 app.use('/api/v1/posts', posts);
 
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, console.log(`Server running ${process.env.NODE_ENV
-} mode on port ${PORT}`))
+app.listen(
+  PORT,
+  console.log(`Server running ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
