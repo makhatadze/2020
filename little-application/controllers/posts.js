3495ceb5bf8968b50bb160ead4fe0a9b31ff1477
makhatadze <vitomaxatadze@gmail.com>
2020-01-09 13:40:00 +0400

    Create controller and route

---
+++ little-application/controllers/posts.js
@@ -0,0 +1,47 @@
+// @desc   Get all Posts
+// @route  Get /api/v1/posts
+// @access Public
+exports.getPosts = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        msg: 'Show all posts',
+    })
+}
+
+// @desc   Get signle Post
+// @route  Get /api/v1/posts/:id
+// @access Public
+exports.getPost = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        msg: 'Get  post'
+    })
+}
+
+// @desc   Create new Post
+// @route  Post /api/v1/posts/:id
+// @access Private
+exports.createPost = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        msg: 'Create new post'
+    })
+}
+// @desc   Update  Post
+// @route  PUT /api/v1/posts/:id
+// @access Private
+exports.updatePost = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        msg: 'Update  post'
+    })
+}
+// @desc   Delete Post
+// @route  DELETE /api/v1/posts/:id
+// @access Private
+exports.deletePost = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        msg: 'Delete post'
+    })
+}
--- little-application/server.js
+++ little-application/server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const dotenv = require('dotenv')
 
+
+// Route
+const posts = require('./routes/posts')
+
 // Load env vars
 dotenv.config({
     path: './config/config.env'
@@ -8,6 +12,17 @@ dotenv.config({
 
 const app = express()
 
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`)
+    next()
+}
+
+app.use(logger)
+// Mount roouters
+app.use('/api/v1/posts', posts);
+
+
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, console.log(`Server running ${process.env.NODE_ENV
+} mode on port ${PORT}`))
