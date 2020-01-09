9c890c3c78fc6cbd2071cae569c25e7fc63d524c
makhatadze <vitomaxatadze@gmail.com>
2020-01-09 11:54:34 +0400

    Create http request

--- vuex/src/store/index.js
+++ vuex/src/store/index.js
@@ -1,6 +1,7 @@
 import Vuex from 'vuex'
 import Vue from 'vue'
 import todos from './modules/todos'
+Node.js, Express, MongoDB & More: The Complete Bootcamp 2020Node.js, Express, MongoDB & More: The Complete Bootcamp 2020
 
 //Vuex
 
