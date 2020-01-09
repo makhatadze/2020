97152ce20745f63f9c7610fa5679497400cddf26
makhatadze <vitomaxatadze@gmail.com>
2020-01-09 12:16:05 +0400

    Return status code and data

--- req-HTTP/server.js
+++ req-HTTP/server.js
@@ -1,15 +1,39 @@
-const http = require('http')
+const http = require('http');
 
+const datas = [
+  {
+    id: 1,
+    text: 'DAta One'
+  },
+  {
+    id: 3,
+    text: 'Data Two'
+  },
+  {
+    id: 2,
+    text: 'Data Tree'
+  }
+];
 const server = http.createServer((req, res) => {
-    const {
-        headers,
-        url,
-        method
-    } = req
-    console.log(headers, url, method)
-    res.end()
-})
+  //   res.statusCode = 404;
+  //   res.setHeader('Content-Type', 'application/json');
+  //   res.setHeader('X-Powered-By', 'Node.js');
+  // Same thing
+  res.writeHead(404, {
+    'Content-Type': 'application/json',
+    'X-Powered-By': 'Node.js'
+  });
 
-const PORT = 5000
+  // we can return normal status code and data! also
+  res.end(
+    JSON.stringify({
+      success: false,
+      error: 'Not Found',
+      data: null
+    })
+  );
+});
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+const PORT = 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
