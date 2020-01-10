e469f65cda9ccb43e2c5812bd72ccc15a4b55900
makhatadze <vitomaxatadze@gmail.com>
2020-01-10 11:41:55 +0400

    Data importet from json file into Mongodb

---
+++ little-application/seeder.js
@@ -0,0 +1,55 @@
+const fs = require('fs')
+const mongoose = require('mongoose')
+const colors = require('colors')
+const dotenv = require('dotenv')
+
+
+// Load env vars
+dotenv.config({
+    path: './config/config.env'
+})
+
+// Load models
+
+const Post = require('./models/Post')
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+// Read JSON files
+const posts = JSON.parse(fs.readFileSync(`${__dirname}/_data/posts.json`, 'utf-8'))
+
+// Import into Db
+const importData = async () => {
+    try {
+        await Post.create(posts)
+
+        console.log('Data imported...'.green.inverse)
+        process.exit()
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+// Delete data
+const deleteData = async () => {
+    try {
+        await Post.deleteMany()
+
+        console.log('Data Destroyed...'.red.inverse)
+        process.exit()
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+if (process.argv[2] === '-i') {
+    importData()
+} else if (process.argv[2] === '-d') {
+    deleteData()
+}
