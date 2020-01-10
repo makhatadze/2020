1df8c72a70739b8640cd54c6e825690953178a67
makhatadze <vitomaxatadze@gmail.com>
2020-01-10 13:01:23 +0400

    Create next, previous and Pagination

--- little-application/controllers/posts.js
+++ little-application/controllers/posts.js
@@ -15,12 +15,11 @@ exports.getPosts = asyncHandler(async (req, res, next) => {
     }
 
     // Fields to exclude
-    const removeFields = ['select', 'sort']
+    const removeFields = ['select', 'sort', 'page', 'limit']
 
     // Loop over removeFields and delete them from reqQuery
     removeFields.forEach(param => delete reqQuery[param])
 
-    console.log(reqQuery)
 
     // Create query string
     let queryStr = JSON.stringify(reqQuery)
@@ -45,11 +44,40 @@ exports.getPosts = asyncHandler(async (req, res, next) => {
         query = query.sort('-createdAt')
     }
 
+    // Pagination 
+    const page = parseInt(req.query.page, 10) || 1
+    const limit = parseInt(req.query.limit, 10) || 1
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+    const total = await Post.countDocuments()
+
+    query = query.skip(startIndex).limit(limit)
+
+
     // Executing query
     const post = await query
 
+    // Pagination result
+    const pagination = {}
+
+    if (endIndex < total) {
+        pagination.next = {
+            page: page + 1,
+            limit
+        }
+    }
+
+    if (startIndex > 0) {
+        pagination.prev = {
+            page: page - 1,
+            limit
+        }
+    }
+
     res.status(200).json({
         success: true,
+        count: post.length,
+        pagination,
         data: post
     })
 
